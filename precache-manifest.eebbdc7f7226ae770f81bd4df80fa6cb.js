fabb2987a7be17259527e4d4d12d433a08159516
Hun Lee <dan.hun.lee@gmail.com>
2019-08-03 00:09:53 -0700

    Updates

---
+++ precache-manifest.eebbdc7f7226ae770f81bd4df80fa6cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6905bf43bde6d8a8506f8e227e17645",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "51ae25706330744db69c",
+    "url": "/clickygame/static/css/main.05f1ce39.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "51ae25706330744db69c",
+    "url": "/clickygame/static/js/main.3ebcd12c.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
